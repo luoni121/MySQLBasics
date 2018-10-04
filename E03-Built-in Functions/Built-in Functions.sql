c46cd83a21b15687faf10b1bccca911ec9c0e71f
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-04 23:05:47 +0300

    Update Built-in Functions.sql

--- E03-Built-in Functions/Built-in Functions.sql
+++ E03-Built-in Functions/Built-in Functions.sql
@@ -148,24 +148,57 @@ WHERE
 13.	 User Email Providers
 ******************************************************/
 
-
+SELECT 
+    user_name,
+    SUBSTRING(email,
+        POSITION('@' IN email) + 1) AS `Email Provider`
+FROM
+    users
+ORDER BY `Email Provider`, user_name;
 
 
 /******************************************************
 14.	 Get Users with IP Address Like Pattern
 ******************************************************/
 
+SELECT 
+    user_name, ip_address
+FROM
+    users
+    WHERE ip_address LIKE '___.1%.%.___'
+ORDER BY user_name;
 
 
 /******************************************************
 15.	 Show All Games with Duration and Part of the Day
 ******************************************************/
 
-
-
-
+SELECT 
+    name AS `game`,
+    CASE
+        WHEN EXTRACT(HOUR FROM start) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM start) BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN EXTRACT(HOUR FROM start) BETWEEN 18 AND 23 THEN 'Evening'
+        ELSE ''
+    END AS `Part of the Day`,
+CASE
+        WHEN duration BETWEEN 0 AND 3 THEN 'Extra Short'
+        WHEN duration BETWEEN 4 AND 6 THEN 'Short'
+        WHEN duration BETWEEN 7 AND 10 THEN 'Long'
+		WHEN duration IS NULL THEN 'Extra Long'
+        ELSE 'Extra Long'
+    END AS `Duration`
+FROM
+    games;
 
 /******************************************************
 16.	 Orders Table
 ******************************************************/
+
+SELECT 
+product_name, 
+order_date, 
+DATE_ADD(order_date, INTERVAL 3 DAY) AS 'pay_due',
+DATE_ADD(order_date, INTERVAL 1 MONTH) AS 'deliver_due'
+FROM orders;
 
