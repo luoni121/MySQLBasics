53bbd2f6af92f67255fb86ab38d07c555b3d8156
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-04 23:50:50 +0300

    Update Data Aggregation.sql

--- E04-Data Aggregation/Data Aggregation.sql
+++ E04-Data Aggregation/Data Aggregation.sql
@@ -2,17 +2,38 @@
 1. Records’ Count
 *******************************************/
 
+SELECT COUNT(*) AS `count`
+FROM wizzard_deposits;
+
 /*******************************************
 2. Longest Magic Wand
 *******************************************/
+
+SELECT MAX(magic_wand_size) AS `longest_magic_wand`
+FROM wizzard_deposits;
 
 /*******************************************
 3. Longest Magic Wand per Deposit Groups
 *******************************************/
 
+SELECT 
+    deposit_group, MAX(magic_wand_size) AS `longest_magic_wand`
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `longest_magic_wand`, deposit_group;
+
 /*******************************************
 4. Smallest Deposit Group per Magic Wand Size*
 *******************************************/
+
+SELECT 
+    deposit_group
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY AVG(magic_wand_size)
+LIMIT 1;
 
 /*******************************************
 5. Deposits Sum
@@ -70,6 +91,9 @@
 18. Salary Challenge**
 *******************************************/
 
+/*******************************************
+19.	Departments Total Salaries
+*******************************************/
 
 
 
