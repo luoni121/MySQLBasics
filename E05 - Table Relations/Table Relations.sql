58c81d5f2d1a88594580438162fed853da97f715
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-07 15:21:16 +0300

    Update Table Relations.sql

--- E05 - Table Relations/Table Relations.sql
+++ E05 - Table Relations/Table Relations.sql
@@ -53,11 +53,40 @@ VALUES( 101,'X1',1),(102,'i6',1),(103,'Model S',2),(104,'Model X',2),(105,'Model
 3.	Many-To-Many Relationship
 *******************************************/
 
+CREATE TABLE students
+(
+    student_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL 
+);
 
+CREATE TABLE exams
+(
+   exam_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL 
+);
+
+CREATE TABLE students_exams (
+    student_id INT(11) NOT NULL,
+    exam_id INT(11) NOT NULL,
+    CONSTRAINT pk_student_exam PRIMARY KEY (student_id , exam_id),
+    CONSTRAINT fk_students_student_id FOREIGN KEY (student_id) REFERENCES students (student_id),
+    CONSTRAINT fk_exam_exam_id FOREIGN KEY (exam_id) REFERENCES exams (exam_id)
+);
+
+INSERT INTO students(student_id, name)
+VALUES (1,'Mila'), (2,'Toni'), (3,'Ron');
+ 
+INSERT INTO exams(exam_id, name)
+VALUES (101, 'Spring MVC'), (102, 'Neo4j'), (103, 'Oracle 11g');
+ 
+INSERT INTO students_exams(student_id, exam_id)
+VALUES (1, 101), (1, 102), (2, 101), (3, 103), (2, 102), (2, 103);
 
 /*******************************************
 4.	Self-Referencing
 *******************************************/
+
+
 
 /*******************************************
 5.	Online Store Database
