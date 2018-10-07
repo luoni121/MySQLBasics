c557ab89116bbcd8c0ff7d187c4429de6e31a7c3
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-07 20:41:11 +0300

    Update Table Relations.sql

--- E05 - Table Relations/Table Relations.sql
+++ E05 - Table Relations/Table Relations.sql
@@ -86,21 +86,113 @@ VALUES (1, 101), (1, 102), (2, 101), (3, 103), (2, 102), (2, 103);
 4.	Self-Referencing
 *******************************************/
 
+CREATE TABLE teachers (
+ teacher_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(50),
+ manager_id INT(11)
+);
 
+INSERT INTO teachers (teacher_id,name,manager_id)
+VALUES
+(101,'John',NULL),
+(102,'Maya',106),
+(103,'Silvia',106),
+(104,'Ted',105),
+(105,'Mark',101),
+(106,'Greta',101);
+
+ALTER TABLE teachers
+ADD CONSTRAINT fk_teachers_teachers
+FOREIGN KEY (manager_id) REFERENCES teachers(teacher_id);
 
 /*******************************************
 5.	Online Store Database
 *******************************************/
 
+CREATE TABLE cities(
+city_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
+
+CREATE TABLE customers(
+customer_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50),
+birthday DATE,
+city_id INT(11),
+CONSTRAINT fk_customers_cities FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+
+CREATE TABLE orders(
+order_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+customer_id INT(11),
+CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE item_types(
+item_type_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
+
+CREATE TABLE items(
+item_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50),
+item_type_id INT(11),
+CONSTRAINT fk_items_item_types FOREIGN KEY (item_type_id) REFERENCES item_types(item_type_id)
+);
+
+CREATE TABLE order_items(
+order_id INT(11),
+item_id INT(11),
+CONSTRAINT pk_order_items PRIMARY KEY (order_id,item_id),
+CONSTRAINT fk_order_items_orders FOREIGN KEY (order_id) REFERENCES orders(order_id),
+CONSTRAINT fk_order_items_items FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+
 /*******************************************
 6.	University Database
 *******************************************/
 
-/*******************************************
-7.	SoftUni Design
-*******************************************/
+CREATE TABLE subjects (
+subject_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+subject_name VARCHAR(50)
+);
+
+CREATE TABLE majors (
+major_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
+
+CREATE TABLE students (
+student_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+student_number VARCHAR(12),
+student_name VARCHAR(50),
+major_id INT(11),
+CONSTRAINT fk_students_majors FOREIGN KEY (major_id) REFERENCES majors(major_id)
+);
+
+CREATE TABLE payments (
+payment_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+payment_date DATE,
+payment_amount DECIMAL(8,2),
+student_id INT(11),
+CONSTRAINT fk_payments_students FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE agenda (
+student_id INT(11),
+subject_id INT(11),
+PRIMARY KEY(student_id, subject_id),
+CONSTRAINT fk_agenda_students FOREIGN KEY (student_id) REFERENCES students(student_id),
+CONSTRAINT fk_agenda_subjects FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+);
 
 /*******************************************
 9.	Peaks in Rila
 *******************************************/
+
+SELECT m.mountain_range, p.peak_name, p.elevation AS `peak_elevation`
+FROM mountains AS m
+JOIN peaks AS p ON m.id = p.mountain_id
+WHERE m.mountain_range = 'Rila'
+ORDER BY  `peak_elevation` DESC;
 
